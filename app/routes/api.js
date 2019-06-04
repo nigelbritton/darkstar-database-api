e2372a1132367599e11f0cba7fc58abeb92b3354
Nigel Complete Control <nigel@completecontrol.co.uk>
2019-06-04 17:32:11 +0100

    Database Layers
    Added cache layer to API endpoints

--- app/routes/api.js
+++ app/routes/api.js
@@ -8,36 +8,86 @@ var express = require('express'),
     debug = require('debug')('darkstar-database-api'),
     router = express.Router();
 
+let applicationStatus = {
+    version: require('../../package.json').version,
+    name: require('../../package.json').name,
+    serverPort: process.env.PORT || 5000,
+    environment: process.env.NODE_ENV || 'development',
+    started: new Date()
+};
+
 var dataContent = require('../lib/dataContent');
 
 router.get('/getPlayers', function (req, res, next) {
-    dataContent.query('select charid, charname, nation, pos_zone, playtime from chars;')
-    .then(function (results) {
-        res.send(results);
-    })
-    .catch(function () {
-        res.send({});
-    });
+    let cachedContent = dataContent.getCacheData('getPlayers');
+    if (!cachedContent) {
+        dataContent.query('select charid, charname, nation, pos_zone, playtime from chars;')
+            .then(function (results) {
+                dataContent.setCacheData('getPlayers', results, 7200);
+                res.send({
+                    version: applicationStatus.version,
+                    data: results,
+                    updated: new Date().getTime()
+                });
+            })
+            .catch(function () {
+                res.send({});
+            });
+    } else {
+        res.send({
+            version: applicationStatus.version,
+            data: cachedContent,
+            updated: new Date().getTime()
+        });
+    }
 });
 
 router.get('/getAuctionListings', function (req, res, next) {
-    dataContent.query('select itemid, stack, seller, seller_name, date, buyer_name, sale, sell_date from auction_house where sell_date = 0;')
-    .then(function (results) {
-        res.send(results);
-    })
-    .catch(function () {
-        res.send({});
-    });
+    let cachedContent = dataContent.getCacheData('getAuctionListings');
+    if (!cachedContent) {
+        dataContent.query('select ah.itemid, ah.stack, ah.seller, ah.seller_name, ah.date, ah.buyer_name, ah.sale, ah.sell_date, ia.level, ia.jobs, ib.name, ib.aH from auction_house ah left join item_armor ia on ah.itemid = ia.itemid left join item_basic ib on ah.itemid = ib.itemid  where ah.sell_date = 0;')
+            .then(function (results) {
+                dataContent.setCacheData('getAuctionListings', results, 7200);
+                res.send({
+                    version: applicationStatus.version,
+                    data: results,
+                    updated: new Date().getTime()
+                });
+            })
+            .catch(function () {
+                res.send({});
+            });
+    } else {
+        res.send({
+            version: applicationStatus.version,
+            data: cachedContent,
+            updated: new Date().getTime()
+        });
+    }
 });
 
 router.get('/getAuctionHistory', function (req, res, next) {
-    dataContent.query('select itemid, stack, seller, seller_name, date, buyer_name, sale, sell_date from auction_house where sell_date <> 0;')
-    .then(function (results) {
-        res.send(results);
-    })
-    .catch(function () {
-        res.send({});
-    });
+    let cachedContent = dataContent.getCacheData('getAuctionHistory');
+    if (!cachedContent) {
+        dataContent.query('select ah.itemid, ah.stack, ah.seller, ah.seller_name, ah.date, ah.buyer_name, ah.sale, ah.sell_date, ia.level, ia.jobs, ib.name, ib.aH from auction_house ah left join item_armor ia on ah.itemid = ia.itemid left join item_basic ib on ah.itemid = ib.itemid where ah.sell_date <> 0;')
+            .then(function (results) {
+                dataContent.setCacheData('getAuctionHistory', results, 7200);
+                res.send({
+                    version: applicationStatus.version,
+                    data: results,
+                    updated: new Date().getTime()
+                });
+            })
+            .catch(function () {
+                res.send({});
+            });
+    } else {
+        res.send({
+            version: applicationStatus.version,
+            data: cachedContent,
+            updated: new Date().getTime()
+        });
+    }
 });
 
 module.exports = router;
